b5dc7b8ee3dd39a542a527e3ac04aad0b0bfc63e
Neto test <2001107@mail.ru>
2024-01-23 17:49:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 56_000;
+
+        int y = 20;
+
+        int bonus = x / y;
+
+        System.out.println("Количество бонусных милей: " + bonus);
+
+    }
+}
